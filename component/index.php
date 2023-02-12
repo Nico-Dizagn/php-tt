205277cdbbf3f00cee5ec7b878cfcc170cd429ee
nicolas <n.namont@agencefove.com>
2023-02-12 22:14:53 +0100

    Add score model

--- component/index.php
+++ component/index.php
@@ -1,24 +1,27 @@
-<?php 
+<?php
 /**
  * 
  */
  class index {
 
     protected $m_sStep = 1 ;
+    protected $m_oForm;
 
     // Controller
     public function __construct(){
+        require_once('../model/form.php');
+        $this->m_oForm = new Form();
+        
         // 
         if($_SERVER['REQUEST_METHOD'] == "POST"){
+            $this->m_oForm->checkForm();
             $this->m_sStep = 2 ;
         }
     }
 
     // View
     public function __toString(){
-
         // model
-        require_once('../model/form.php');
         $l_iQNum = 0;
         $l_sString =  
 '<html>
@@ -30,29 +33,39 @@
     <body>
     <div id="main">
         <h1>Technical test PHP '.date('Y').'</h1>
-        <div>Welcome to this PHP technical test for developers of all levels. <br/>Several skills will be tested in a common PHP core. Multiple answers allowed for each question.</div>
+        <div id="intro">
+            <p>Welcome to this PHP technical test for developers of all levels. Several skills will be tested in a common PHP core.</p>
+            <p>Each correct answer earns one point and each wrong remove one. Multiple answers allowed 
+             for each question.</p>
+        </div>
         ';
         if($this->m_sStep == 2){
-            var_dump($_POST);
+            $l_sString .= '
+        <fieldset id="result">
+            <legend>Result</legend>
+            Total score : <span id="score">'.$this->m_oForm->m_iScore.' / 20</span> ( Right : '.$this->m_oForm->m_iRight.' / Wrong : '.$this->m_oForm->m_iWrong.' ) 
+        </fieldset>' ;
         }
 
         $l_sString .=
         '<form action="" method="POST">';
-        foreach($l_aForm AS $l_aQuestion){
+        foreach($this->m_oForm->getForm() AS $l_aQuestion){
             $l_sString .='
             <div class="question">
                 <fieldset>
                     <legend>#'.++$l_iQNum.' - '.$l_aQuestion['title'].'</legend>                    
                     <p class="desc"> '.$l_aQuestion['desc'].'</p>';
                     foreach($l_aQuestion['answer'] AS $l_sId => $l_aAnswer){
+                        $checked = (TRUE == in_array($l_sId, $_POST)) ? 'checked="checked"': '';
                         $l_sString .='
                     <label class="answer">
                         <input type="checkbox" 
                                 id="'.$l_sId.'" 
                                 name="'.$l_sId.'" 
-                                value="'.$l_sId.'"/>
+                                value="'.$l_sId.'"
+                                '.$checked.'/>
                         <span class="checkmark"></span>        
-                        '.htmlentities($l_aAnswer['label']).'
+                        &nbsp;&nbsp;'.htmlentities($l_aAnswer['label']).'
                     </label>';
                     }
             $l_sString .='
@@ -63,7 +76,7 @@
         $l_sString .= '
             <div id="submit">
                 <button class="button">
-                    <span>Evaluer mes réponses </span>
+                    <span>Evaluate my answers </span>
                 </button>
             </div>
         </form>
@@ -73,6 +86,4 @@
 
         return $l_sString;
     }
-
- }
-
+}
